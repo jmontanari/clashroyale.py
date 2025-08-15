f5e574d420f59d5709e50c75f03f5b20bef257ab
Joe <jmontanari1@gmail.com>
2025-08-15 13:37:09 -0400

    add clashRoyaleClient to the init

--- clashroyale/__init__.py
+++ clashroyale/__init__.py
@@ -106,6 +106,10 @@ from clashroyale.api.locations_api import LocationsApi as LocationsApi
 from clashroyale.api.players_api import PlayersApi as PlayersApi
 from clashroyale.api.tournaments_api import TournamentsApi as TournamentsApi
 
+# import client
+from clashroyale.client.clash_royale_client import ClashRoyaleClient as ClashRoyaleClient
+
+
 # import ApiClient
 from clashroyale.api_response import ApiResponse as ApiResponse
 from clashroyale.api_client import ApiClient as ApiClient
---
+++ clashroyale/client/__init__.py
@@ -0,0 +1,1 @@
+from clashroyale.client.clash_royale_client import ClashRoyaleClient as ClashRoyaleClient
